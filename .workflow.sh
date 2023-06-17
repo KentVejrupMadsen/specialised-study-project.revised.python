cc9602014b247678e1e81cb0909f72c5053ff02b
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-17 20:03:32 +0200

    automating tests

--- .workflow.sh
+++ .workflow.sh
@@ -3,9 +3,10 @@
 python -m venv .venv
 source ./.venv/bin/activate
 
-
-
 export dataset_at=''
 export location_to_application=''
+
+# Load required submodules
+git submodule update --recursive
 
 pytest -s
