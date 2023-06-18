2b7a7f04588a3917d164f974493fe13c18cab0e8
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-18 19:13:09 +0200

    refactored

--- ssp/variables/labels.py
+++ ssp/variables/labels.py
@@ -1,8 +1,12 @@
+training_label: str = 'training'
+test_label: str = 'test'
 
 
 def get_training_label() -> str:
-    return 'training'
+    global training_label
+    return training_label
 
 
 def get_test_label() -> str:
-    return 'test'
+    global test_label
+    return test_label
