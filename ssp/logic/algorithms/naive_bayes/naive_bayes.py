d181bb30ee6192d8ef5ebea1b01d49ad64b6a25d
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-20 21:56:27 +0200

    progress
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

--- ssp/logic/algorithms/naive_bayes/naive_bayes.py
+++ ssp/logic/algorithms/naive_bayes/naive_bayes.py
@@ -6,6 +6,7 @@ from ssp.logic.templates \
 # https://www.javatpoint.com/machine-learning-naive-bayes-classifier
 # https://www.geeksforgeeks.org/naive-bayes-classifiers/
 # https://www.analyticsvidhya.com/blog/2017/09/naive-bayes-explained/
+# https://www.turing.com/kb/an-introduction-to-naive-bayes-algorithm-for-beginners
 class NaiveBayes(FunctionClass):
     def __init__(self):
         super().__init__()
