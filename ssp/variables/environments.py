24ba3c3be6e1b61d9220ce398cfb7addace725a4
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-20 12:31:04 +0200

    current structure

--- ssp/variables/environments.py
+++ ssp/variables/environments.py
@@ -6,3 +6,11 @@ def get_environment_dataset_location() -> str:
     global environment_dataset_location
     return environment_dataset_location
 
+
+def set_environment_dataset_location(
+        value: str
+) -> None:
+    global environment_dataset_location
+    environment_dataset_location = value
+
+
