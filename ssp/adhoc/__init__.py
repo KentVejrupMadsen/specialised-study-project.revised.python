064ea945790c1cfd0245649be00dd5ee73109e22
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-20 08:31:56 +0200

    refactored
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

---
+++ ssp/adhoc/__init__.py
@@ -0,0 +1,4 @@
+from os.path \
+    import \
+    isfile, \
+    isdir
--- ssp/logic/algorithms/__init__.py
+++ ssp/logic/algorithms/__init__.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 from ssp.logic.algorithms.function_class \
     import FunctionClass
+
+from ssp.logic.algorithms.smoothing \
+    import LaplaceSmoothing
+
+from ssp.logic.algorithms.naive \
+    import NaiveBayes
