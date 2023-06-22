fad9e9b0b202130a1c3ca8d507f9351779cece01
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-22 21:26:11 +0200

    working on dataset factory
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

---
+++ ssp/persistence/dataset_map_builder.py
@@ -0,0 +1,73 @@
+from ssp.persistence \
+    import \
+    DataSetMap, \
+    CategoryMap
+
+from os \
+    import listdir, walk
+
+from os.path \
+    import isdir, isfile, join
+
+
+class DataSetMapBuilder:
+    def __init__(
+            self,
+            dataset_location: str
+    ):
+        self.location: str = dataset_location
+        self.dataset: None | DataSetMap = None
+
+        if not isdir(self.location):
+            raise Exception('location not found')
+
+    def __del__(self):
+        del self.location
+
+    def run(self) -> DataSetMap:
+        self.set_dataset(DataSetMap())
+
+        self.generate_categories()
+
+        return self.get_dataset()
+
+    def generate_categories(self):
+        found_items: list = listdir(self.get_location())
+
+        for entry in found_items:
+            full_path: str = join(self.location, entry)
+
+            if isdir(full_path):
+                cm: CategoryMap = self.get_dataset().create(entry)
+                self.search_for_files(cm, full_path)
+
+    def search_for_files(
+            self,
+            cm: CategoryMap,
+            full_path
+    ):
+        for root, dirs, files in walk(
+                full_path,
+                topdown=False
+        ):
+            for selected_file in files:
+                path_to_file: str = join(root, selected_file)
+                cm.insert(path_to_file)
+
+    def get_location(self) -> str:
+        return self.location
+
+    def set_location(
+            self,
+            value: str
+    ) -> None:
+        self.location = value
+
+    def get_dataset(self) -> DataSetMap | None:
+        return self.dataset
+
+    def set_dataset(
+            self,
+            value: DataSetMap | None
+    ) -> None:
+        self.dataset = value
