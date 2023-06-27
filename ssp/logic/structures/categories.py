d0b051464b9e0650837c01bce675996b1cdefdb8
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-28 00:38:38 +0200

    progress
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

--- ssp/logic/structures/categories.py
+++ ssp/logic/structures/categories.py
@@ -15,6 +15,7 @@ class Category(BagOfWords):
     ):
         super().__init__()
         self.category_name: str = category_name
+
         self.tokens: list | None = None
         self.iterator: CounterObject | None = None
 
@@ -22,7 +23,8 @@ class Category(BagOfWords):
         super().__del__()
         del                         \
             self.category_name,     \
-            self.tokens
+            self.tokens,            \
+            self.iterator
 
     def get_name(self) -> str:
         return self.category_name
@@ -53,7 +55,84 @@ class Category(BagOfWords):
             self,
             token: str
     ):
-        pass
+        if self.is_empty():
+            self.create_category_token(
+                token
+            )
+        else:
+            if self.exist_token_by_string(
+                token
+            ):
+                self.increase_token_counter_for(
+                    token
+                )
+            else:
+                self.create_category_token(
+                    token
+                )
+
+    def increase_token_counter_for(
+            self,
+            token_name
+    ) -> bool:
+        hash_of_input_token: int = hash(token_name)
+
+        for index in iter(self):
+            current_token: CategoryToken = self.retrieve_token_at(index)
+            if hash_of_input_token == current_token.get_hash():
+                if token_name == current_token.get_word():
+                    current_token.get_counter().increment()
+                    return True
+        return False
+
+    def exist_token_by_string(
+            self,
+            value: str
+    ) -> bool:
+        hash_of_input_value: int = hash(value)
+        for index in iter(self):
+            current_token: CategoryToken = self.retrieve_token_at(index)
+            if hash_of_input_value == current_token.get_hash():
+                if value == current_token.get_word():
+                    return True
+        return False
+
+    def retrieve_token_at(
+            self,
+            index: int
+    ) -> CategoryToken:
+        return self.get_tokens()[index]
+
+    def remove_token_at(
+            self,
+            index: int
+    ):
+        return self.get_tokens()[index]
+
+    def create_category_token(
+            self,
+            value: str
+    ):
+        self.insert_category_token(
+            CategoryToken(
+                word=value
+            )
+        )
+
+    def insert_category_token(
+            self,
+            value: CategoryToken
+    ) -> None:
+        self.get_tokens().append(
+            value
+        )
+
+    def is_empty(self) -> bool:
+        return bool(
+            self.is_tokens_none()
+            or
+            len(self) == 0
+        )
 
     def get_iterator(self) -> CounterObject:
         if self.is_tokens_none():
