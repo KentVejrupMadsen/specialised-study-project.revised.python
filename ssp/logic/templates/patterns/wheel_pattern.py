154d6ebe902a5b79ab9fca869dba61ebef7fb859
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-28 19:25:52 +0200

    working on patterns
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

--- ssp/logic/templates/patterns/wheel_pattern.py
+++ ssp/logic/templates/patterns/wheel_pattern.py
@@ -1,6 +1,37 @@
+from HardenedSteel.objects  \
+    import CounterObject
+
+
 class WheelPattern:
     def __init__(self):
-        pass
+        self.selection: None | CounterObject = None
 
     def __del__(self):
-        pass
+        del self.selection
+
+    def set_selection(
+        self,
+        value: int
+    ):
+        self.get_selection_counter().set_value(
+            value
+        )
+
+    def get_selection(self) -> int:
+        return self.get_selection_counter().get_value()
+
+    def get_selection_counter(self) -> CounterObject:
+        if self.is_selection_counter():
+            self.set_selection_counter(
+                CounterObject()
+            )
+        return self.selection
+
+    def set_selection_counter(
+            self,
+            value: CounterObject
+    ) -> None:
+        self.selection = value
+
+    def is_selection_counter(self) -> bool:
+        return self.selection is None
