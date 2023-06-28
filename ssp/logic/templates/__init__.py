e3014f51fe54e9cba80b6d97c33f23a86e61e4bc
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-28 19:13:47 +0200

    refactored
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

--- ssp/logic/templates/__init__.py
+++ ssp/logic/templates/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from ssp.logic.templates.patterns       \
-    import FactoryPattern
-
+    import                              \
+    BuilderPattern,                     \
+    FactoryPattern
 
 from ssp.logic.templates.events         \
     import                              \
--- ssp/logic/templates/patterns/__init__.py
+++ ssp/logic/templates/patterns/__init__.py
@@ -1,2 +1,5 @@
 from ssp.logic.templates.patterns.factory_pattern \
     import FactoryPattern
+
+from ssp.logic.templates.patterns.builder_pattern \
+    import BuilderPattern
