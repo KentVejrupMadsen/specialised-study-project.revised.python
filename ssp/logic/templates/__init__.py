55596196de4750972c3f85c76f61a6418b73e4f4
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-28 18:58:59 +0200

    refactored
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

--- ssp/logic/templates/__init__.py
+++ ssp/logic/templates/__init__.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
-from abc                                    \
-    import                                  \
-    ABC,                                    \
-    abstractmethod
-
-from ssp.logic.templates.tokens             \
+from logic.templates.objects.tokens \
     import Token
 
-from ssp.logic.templates.function_class     \
+from logic.templates.events.function_class \
     import FunctionClass
 
-from ssp.logic.templates.factory_pattern    \
+from logic.templates.patterns.factory_pattern \
     import Factory
 
-from ssp.logic.templates.fire_event         \
+from logic.templates.events.fire_event \
     import OnFire
 
-from ssp.logic.templates.bag_of_words       \
-    import BagOfWords
